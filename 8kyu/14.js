011bf2d5fc001616f60bf55d7c3bd4ce04ce5ed2
Max3lord <max3.pav.loff@gmail.com>
2022-12-05 16:07:21 +0500

    add: 14js

---
+++ 8kyu/14.js
@@ -0,0 +1,8 @@
+// Safen User Input Part I - htmlspecialchars
+
+function htmlspecialchars(formData) {
+	return formData.replace(/&/g, "&amp;")
+						.replace(/"/g, "&quot;")
+						.replace(/</g, "&lt;")
+						.replace(/>/g, "&gt;");
+ }
